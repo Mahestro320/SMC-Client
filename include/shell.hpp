c3a2e1b8fff64a30d3a3b16f1087e55450b4b3cd
Mahestro320 <142752255+Mahestro320@users.noreply.github.com>
2025-07-15 22:53:28 +0200

    Uploading files of the new version 0.3.1

--- include/shell.hpp
+++ include/shell.hpp
@@ -19,7 +19,7 @@ class Shell final {
     std::string getCommandInputStartInfo() const;
 
   public:
-    static inline constexpr Version VERSION{0, 3, 0};
+    static inline constexpr Version VERSION{0, 3, 1};
 
     Shell(Client& client);
 
